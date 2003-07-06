0f64d81b93501900d993c6d5187bfca8cc5dfb11
Paolo Bacchilega <paolo.bacch@tin.it>
2003-07-06 18:02:43 +0000

    [ gthumb 2.1.3 released ]
    2003-07-06  Paolo Bacchilega  <paolo.bacch@tin.it>
    
            [ gthumb 2.1.3 released ]
    
    2003-07-06  Paolo Bacchilega  <paolo.bacch@tin.it>
    
            * data/albumthemes/*/*.gthtml: updated to allow the user to
            personalize the captions.
    
            * src/gthumb-ui.xml: fix bug #116386.
    
            * libgthumb/gthumb-init.c (gthumb_init): preload some gconf dirs.
    
            * libgthumb/gtk-utils.c (_gtk_yesno_dialog_new)
            (_gtk_request_dialog_run): no title for dialogs.
    
            * libgthumb/preferences.h:
            * src/dlg-web-exporter.c (show_caption_dialog_cb)
            (caption_dialog__ok_clicked): load and save preferences.

--- libgthumb/print-callbacks.c
+++ libgthumb/print-callbacks.c
@@ -788,7 +788,9 @@ update_comment_font (DialogData *data)
 		g_object_unref (pi->font_comment);		
 
 	font_name = gnome_print_font_picker_get_font_name (GNOME_PRINT_FONT_PICKER (data->comment_fontpicker));
+#ifdef DEBUG
 	g_print ("Find closest: %s\n", font_name);
+#endif
 	pi->font_comment = gnome_font_find_closest_from_full_name (font_name);
 
 	if (pi->font_comment == NULL)
