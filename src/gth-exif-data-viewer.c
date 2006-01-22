47278cbd1e817f210ef81877383dc5a94abb1231
Paolo Bacchilega <paobac@cvs.gnome.org>
2006-01-22 12:03:08 +0000

    Fixed bug #326879 - gthumb-2.6.8: fix compilation against libexif-0.6.13
    2006-01-22  Paolo Bacchilega  <paobac@cvs.gnome.org>
    
            * src/dlg-jpegtran.c:
            * src/gth-exif-utils.h:
            * src/gth-exif-data-viewer.c:
    
            Fixed bug #326879 - gthumb-2.6.8: fix compilation against libexif-0.6.13        Patch by Thomas Klausner

--- src/gth-exif-data-viewer.c
+++ src/gth-exif-data-viewer.c
@@ -34,9 +34,9 @@
 #include "image-viewer.h"
 
 #ifdef HAVE_LIBEXIF
-#include <exif-data.h>
-#include <exif-content.h>
-#include <exif-entry.h>
+#include <libexif/exif-data.h>
+#include <libexif/exif-content.h>
+#include <libexif/exif-entry.h>
 #endif /* HAVE_LIBEXIF */
 
 
