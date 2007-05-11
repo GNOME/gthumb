14bde21f4b69efa2083e2eb00642b4018feb35e0
Michael J. Chudobiak <mjc@cvs.gnome.org>
2007-05-11 12:50:59 +0000

    Don't copy pixbuf, just ref it. This reverts to trunk behaviour.
    2007-05-11  Michael J. Chudobiak  <mjc@cvs.gnome.org>
    
            * libgthumb/image-loader.c: (image_loader_sync_pixbuf):
            Don't copy pixbuf, just ref it. This reverts to trunk behaviour.
    
    
    svn path=/branches/metadata-ideas/; revision=1646

--- libgthumb/image-loader.c
+++ libgthumb/image-loader.c
@@ -490,8 +490,10 @@ image_loader_sync_pixbuf (ImageLoader *il)
 		g_object_unref (priv->pixbuf);
 		priv->pixbuf = NULL;
 	}
-	if (pixbuf != NULL)
-		priv->pixbuf = gdk_pixbuf_copy (pixbuf);
+	if (pixbuf != NULL) {
+		g_object_ref (pixbuf);
+		priv->pixbuf = pixbuf;
+	}
 
 	g_mutex_unlock (priv->yes_or_no);
 }
