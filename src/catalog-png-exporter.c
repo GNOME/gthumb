c22a7cd7217c178f511c780b623c218e8c62c1fe
Michael J. Chudobiak <mjc@src.gnome.org>
2007-06-22 16:56:47 +0000

    Merged 1708:1709 from trunk.
    svn path=/branches/metadata-ideas/; revision=1710

--- src/catalog-png-exporter.c
+++ src/catalog-png-exporter.c
@@ -102,7 +102,7 @@ static char *get_header_text                (CatalogPngExporter *ce,
 
 #define HTML_PAGE_BEGIN "\
 <?xml version=\"1.0\" encoding=\"utf-8\"?>\n\
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n\ 
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n\
   \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n\
 <html xmlns=\"http://www.w3.org/1999/xhtml\">\n\
 <head>\n\
