a07c82e8a9c01c96795b68c30989336a0a07f0df
Michael J. Chudobiak <mjc@cvs.gnome.org>
2007-07-20 15:33:13 +0000

    Added code to extract video DateTime tags using mplayer's midentify tool,
    2007-07-20  Michael J. Chudobiak  <mjc@cvs.gnome.org>
    
            * NEWS:
            * libgthumb/file-data.c: (file_data_load_exif_data):
            * libgthumb/gth-exif-utils.c: (get_mplayer_time):
            * libgthumb/gth-exif-utils.h:
            Added code to extract video DateTime tags using mplayer's
            midentify tool, if present.
    
    
    svn path=/branches/gthumb-2-10/; revision=1816

--- libgthumb/gth-exif-utils.h
+++ libgthumb/gth-exif-utils.h
@@ -49,6 +49,7 @@ char *        get_exif_tag                (const char   *filename,
 ExifShort     get_exif_tag_short          (const char   *filename,
 				           ExifTag       etag);
 time_t        get_exif_time               (const char   *filename);
+time_t        get_mplayer_time            (const char   *filename);
 char *        get_exif_aperture_value     (const char   *filename);
 gboolean      have_exif_time              (const char   *filename);
 const char   *get_exif_entry_value        (ExifEntry    *entry);
