f36b08c5fc78b5f5bcd3a32994fc10d298d26059
Michael J. Chudobiak <mjc@svn.gnome.org>
2007-10-28 23:47:08 +0000

    Make the cropping dialog more intuitive, so my mom can use it. Bug
    2007-10-28  Michael J. Chudobiak  <mjc@svn.gnome.org>
    
            * NEWS:
            * data/glade/gthumb_crop.glade:
            * src/dlg-crop.c: (dlg_crop):
            Make the cropping dialog more intuitive, so my mom can use it.
            Bug #408342. Trunk-only experimental change.
    
    
    svn path=/trunk/; revision=2031

--- src/dlg-crop.c
+++ src/dlg-crop.c
@@ -117,14 +117,6 @@ apply_cb (DialogData *data,
 
 
 static void
-ok_cb (GtkWidget  *widget,
-       DialogData *data)
-{
-	apply_cb (data, FALSE);
-}
-
-
-static void
 save_cb (GtkWidget  *widget,
 	 DialogData *data)
 {
@@ -449,7 +441,6 @@ void
 dlg_crop (GthWindow *window)
 {
 	DialogData   *data;
-	GtkWidget    *ok_button;
 	GtkWidget    *save_button;
 	GtkWidget    *cancel_button;
 	GtkWidget    *help_button;
@@ -497,10 +488,9 @@ dlg_crop (GthWindow *window)
 	zoom_fit_button = glade_xml_get_widget (data->gui, "crop_zoom_fit_button");
 	data->mask_button = glade_xml_get_widget (data->gui, "crop_showmask_togglebutton");
 
-	ok_button = glade_xml_get_widget (data->gui, "crop_okbutton");
-	save_button = glade_xml_get_widget (data->gui, "crop_savebutton");
-	cancel_button = glade_xml_get_widget (data->gui, "crop_cancelbutton");
-	help_button = glade_xml_get_widget (data->gui, "crop_helpbutton");
+	save_button = glade_xml_get_widget (data->gui, "crop_save_button");
+	cancel_button = glade_xml_get_widget (data->gui, "crop_cancel_button");
+	help_button = glade_xml_get_widget (data->gui, "crop_help_button");
 
 	data->image_selector = gth_image_selector_new (GTH_SELECTOR_TYPE_REGION, NULL);
 
@@ -568,10 +558,6 @@ dlg_crop (GthWindow *window)
 				  "clicked",
 				  G_CALLBACK (gtk_widget_destroy),
 				  data->dialog);
-	g_signal_connect (G_OBJECT (ok_button),
-			  "clicked",
-			  G_CALLBACK (ok_cb),
-			  data);
 	g_signal_connect (G_OBJECT (save_button),
 			  "clicked",
 			  G_CALLBACK (save_cb),
