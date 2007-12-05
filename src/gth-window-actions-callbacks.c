ba0f0d3a3a2ed298adb102e3d01429d23d02fcec
Michael J. Chudobiak <mjc@svn.gnome.org>
2007-12-05 16:11:33 +0000

    Update credits to match AUTHORS file.
    2007-12-05  Michael J. Chudobiak  <mjc@svn.gnome.org>
    
            * src/gth-window-actions-callbacks.c:
            (gth_window_activate_action_help_about):
            Update credits to match AUTHORS file.
    
    
    svn path=/branches/gthumb-2-10/; revision=2092

--- src/gth-window-actions-callbacks.c
+++ src/gth-window-actions-callbacks.c
@@ -878,7 +878,20 @@ gth_window_activate_action_help_about (GtkAction *action,
 {
 	GthWindow  *window = GTH_WINDOW (data);
 	const char *authors[] = {
-		"Paolo Bacchilega <paolo.bacchilega@libero.it>",
+		"Paolo Bacchilega <paobac@svn.gnome.org>",
+		"Michael J. Chudobiak <mjc@svn.gnome.org>",
+		"",
+		"Josua Groeger <fractalus@gmx.net>",
+		"Chauk-Mean Proum <chaukmean@free.fr>",
+		"Jason Tackaberry <tack@auc.ca>",
+		"Martin Ebourne <martin@zepler.org>",
+		"Manuel Braga <mnulbga@gmail.com>",
+		"Jef Driesen <jefdriesen@hotmail.com>",
+		"Rennie deGraaf <degraaf@cpsc.ucalgary.ca>",
+		"Joakim Larsson <joakim@bildrulle.nu>",
+		"Christophe Bisière <bisiere@univ-tlse1.fr>",
+		"Ignacy Gawędzki <ibugs@qult.net>",
+		"Geoffrey Antos <dynamotwain@aim.com>",
 		NULL
 	};
 	const char *documenters [] = {
