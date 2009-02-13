b8eb295ec66af5e8863e1c8903952c3a753b9121
Michael J. Chudobiak <mjc@src.gnome.org>
2009-02-13 18:33:38 +0000

    Merged 2506:2507 from gthumb-2-10.
    svn path=/trunk/; revision=2508

--- libgthumb/image-loader.c
+++ libgthumb/image-loader.c
@@ -142,8 +142,6 @@ image_loader_finalize__step2 (GObject *object)
 	g_mutex_free (priv->start_loading_mutex);
 	g_mutex_free (priv->exit_thread_mutex);
 
-	g_free (priv);
-	il->priv = NULL;
 
 	/* Chain up */
 	G_OBJECT_CLASS (parent_class)->finalize (object);
@@ -187,6 +185,9 @@ image_loader_finalize (GObject *object)
 				  object,
 				  FALSE,
 				  FALSE);
+        g_free (priv);
+        il->priv = NULL;
+
 }
 
 
