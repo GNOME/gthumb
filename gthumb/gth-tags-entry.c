a4b16a8bebc644f95179c9b1a391768b7709cd96
Funda Wang <fundawang@gmail.com>
2010-01-24 04:15:39 +0100

    Build with gtk > 2.19.3 (GTK_WIDGET_TOPLEVEL -> gtk_widget_is_toplevel)

--- gthumb/gth-tags-entry.c
+++ gthumb/gth-tags-entry.c
@@ -614,7 +614,11 @@ tag_list_unmap_cb (GtkWidget    *widget,
         GdkGeometry  geometry;
 
         toplevel = gtk_widget_get_toplevel (widget);
-        if (! GTK_WIDGET_TOPLEVEL (toplevel))
+#if GTK_CHECK_VERSION (2, 19, 3)
+	if (! gtk_widget_is_toplevel (toplevel))
+#else
+	if (! GTK_WIDGET_TOPLEVEL (toplevel))
+#endif
         	return;
 
         geometry.max_height = -1;
