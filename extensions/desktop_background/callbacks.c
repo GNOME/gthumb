2e7e8a4397049f34115712f3329dc464da1d47b4
Christophe Bisière <bisiere@univ-tlse1.fr>
2010-05-13 11:20:47 +0200

    Move "Set as Desktop Background" item from "Tools" menu to right-click popup menu
    [bug #618430]

--- extensions/desktop_background/callbacks.c
+++ extensions/desktop_background/callbacks.c
@@ -32,8 +32,13 @@
 
 static const char *fixed_ui_info =
 "<ui>"
-"  <popup name='ListToolsPopup'>"
-"    <placeholder name='Tools'>"
+"  <popup name='FileListPopup'>"
+"    <placeholder name='Screen_Actions'>"
+"      <menuitem name='DesktopBackground' action='Tool_DesktopBackground'/>"
+"    </placeholder>"
+"  </popup>"
+"  <popup name='FilePopup'>"
+"    <placeholder name='Screen_Actions'>"
 "      <menuitem name='DesktopBackground' action='Tool_DesktopBackground'/>"
 "    </placeholder>"
 "  </popup>"
@@ -82,7 +87,8 @@ db__gth_browser_construct_cb (GthBrowser *browser)
 		g_clear_error (&error);
 	}
 
-	gtk_image_menu_item_set_always_show_image (GTK_IMAGE_MENU_ITEM (gtk_ui_manager_get_widget (gth_browser_get_ui_manager (browser), "/ListToolsPopup/Tools/DesktopBackground")), TRUE);
+	gtk_image_menu_item_set_always_show_image (GTK_IMAGE_MENU_ITEM (gtk_ui_manager_get_widget (gth_browser_get_ui_manager (browser), "/FileListPopup/Screen_Actions/DesktopBackground")), TRUE);
+	gtk_image_menu_item_set_always_show_image (GTK_IMAGE_MENU_ITEM (gtk_ui_manager_get_widget (gth_browser_get_ui_manager (browser), "/FilePopup/Screen_Actions/DesktopBackground")), TRUE);
 
 	g_object_set_data_full (G_OBJECT (browser), BROWSER_DATA_KEY, data, (GDestroyNotify) browser_data_free);
 }
