fe5ac822415bc72ab20a7187594a1994033d64d3
Paolo Bacchilega <paobac@src.gnome.org>
2010-09-21 22:00:16 +0200

    show the web services first in the share menu
    create the share menu before the share commands to respect the
    desired order; and create it after the list tools to show it
    as last menu in the toolbar.

--- extensions/export_tools/main.c
+++ extensions/export_tools/main.c
@@ -29,7 +29,7 @@
 G_MODULE_EXPORT void
 gthumb_extension_activate (void)
 {
-	gth_hook_add_callback ("gth-browser-construct", 15, G_CALLBACK (export_tools__gth_browser_construct_cb), NULL);
+	gth_hook_add_callback ("gth-browser-construct", 7, G_CALLBACK (export_tools__gth_browser_construct_cb), NULL);
 }
 
 
