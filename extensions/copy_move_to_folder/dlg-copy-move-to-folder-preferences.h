559c8ead45bfc40e654fb7a0c9580e19b2b6b0fe
Marlodavampire <vworker.com>
2010-11-10 15:25:09 +1100

    copy_move_to_folder: add preference dialog

--- extensions/copy_move_to_folder/dlg-copy-move-to-folder-preferences.h
+++ extensions/copy_move_to_folder/dlg-copy-move-to-folder-preferences.h
@@ -3,7 +3,7 @@
 /*
  *  GThumb
  *
- *  Copyright (C) 2010 Free Software Foundation, Inc.
+ *  Copyright (C) 2010 The Free Software Foundation, Inc.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,37 +19,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef DLG_COPY_MOVE_TO_FOLDER_PREFERENCES_H
+#define DLG_COPY_MOVE_TO_FOLDER_PREFERENCES_H
 
-#include <config.h>
-#include <gtk/gtk.h>
 #include <gthumb.h>
-#include "callbacks.h"
-#include "dlg-copy-move-to-folder-preferences.h"
 
+void dlg_copy_move_to_folder_preferences (GtkWindow *parent);
 
-G_MODULE_EXPORT void
-gthumb_extension_activate (void)
-{
-	gth_hook_add_callback ("gth-browser-construct", 20, G_CALLBACK (db__gth_browser_construct_cb), NULL);
-	gth_hook_add_callback ("gth-browser-update-sensitivity", 10, G_CALLBACK (db__gth_browser_update_sensitivity_cb), NULL);
-}
-
-
-G_MODULE_EXPORT void
-gthumb_extension_deactivate (void)
-{
-}
-
-
-G_MODULE_EXPORT gboolean
-gthumb_extension_is_configurable (void)
-{
-	return TRUE;
-}
-
-
-G_MODULE_EXPORT void
-gthumb_extension_configure (GtkWindow *parent)
-{
-	dlg_copy_move_to_folder_preferences (parent);
-}
+#endif /* DLG_COPY_MOVE_TO_FOLDER_PREFERENCES_H */
