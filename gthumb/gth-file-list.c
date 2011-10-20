80d4f78d5fdf9bd02143ffccd425e6ea5c0e9cbe
Paolo Bacchilega <paobac@src.gnome.org>
2011-10-20 14:31:40 +0200

    fixed resize of the file list

--- gthumb/gth-file-list.c
+++ gthumb/gth-file-list.c
@@ -644,7 +644,7 @@ gth_file_list_construct (GthFileList     *file_list,
 
 	file_list->priv->scrolled_window = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (file_list->priv->scrolled_window),
-					GTK_POLICY_NEVER,
+					GTK_POLICY_AUTOMATIC,
 					GTK_POLICY_AUTOMATIC);
 	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (file_list->priv->scrolled_window),
 					     GTK_SHADOW_ETCHED_IN);
