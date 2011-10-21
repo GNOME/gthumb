a988d2cd93ddc7fcb50e56e0a4f237e179434d1d
Paolo Bacchilega <paobac@src.gnome.org>
2011-10-21 17:40:06 +0200

    center the thumbnail caption horinzontally

--- gthumb/gth-icon-view.c
+++ gthumb/gth-icon-view.c
@@ -336,6 +336,7 @@ gth_icon_view_update_attributes (GthFileView     *self,
 		      "yalign", 0.0,
 		      NULL);
 	g_object_set (text_renderer,
+		      "xalign", 0.5,
 		      "yalign", 0.0,
 		      "alignment", PANGO_ALIGN_CENTER,
 		      "width", thumb_size + THUMBNAIL_BORDER,
