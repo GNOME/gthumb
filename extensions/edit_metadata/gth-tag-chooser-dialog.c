c3ac4057fb469afbb3f773c3f02e775378822310
Paolo Bacchilega <paobac@src.gnome.org>
2011-11-24 21:31:44 +0100

    make the tag dialog resizable

--- extensions/edit_metadata/gth-tag-chooser-dialog.c
+++ extensions/edit_metadata/gth-tag-chooser-dialog.c
@@ -220,7 +220,7 @@ gth_tag_chooser_dialog_init (GthTagChooserDialog *self)
 	self->priv->builder = _gtk_builder_new_from_file ("tag-chooser.ui", "edit_metadata");
 
 	gtk_window_set_title (GTK_WINDOW (self), _("Assign Tags"));
-	gtk_window_set_resizable (GTK_WINDOW (self), FALSE);
+	gtk_window_set_resizable (GTK_WINDOW (self), TRUE);
 	gtk_box_set_spacing (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (self))), 5);
 	gtk_container_set_border_width (GTK_CONTAINER (self), 5);
 
