82cc7e8f746cdf6fdaff2b910adfd9c15294bd75
Paolo Bacchilega <paobac@src.gnome.org>
2012-03-26 20:37:46 +0200

    use the 3 dots unicode character instead of 3 dots

--- gthumb/gth-file-properties.c
+++ gthumb/gth-file-properties.c
@@ -138,7 +138,7 @@ gth_file_properties_real_set_file (GthPropertyView *base,
 			char *tmp_value;
 
 			if (g_utf8_strlen (value, -1) > MAX_ATTRIBUTE_LENGTH)
-				g_utf8_strncpy (g_utf8_offset_to_pointer (value, MAX_ATTRIBUTE_LENGTH - 3), "...", 3);
+				g_utf8_strncpy (g_utf8_offset_to_pointer (value, MAX_ATTRIBUTE_LENGTH - 1), "…", 1);
 			tmp_value = _g_utf8_replace (value, "[\r\n]", " ");
 			g_free (value);
 			value = tmp_value;
