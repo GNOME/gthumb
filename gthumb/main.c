5fad73ff46de6629736f263fb5b1d86de8798066
Paolo Bacchilega <paobac@src.gnome.org>
2012-06-13 12:02:29 +0200

    set register-session to true in order to call gtk_application_inhibit

--- gthumb/main.c
+++ gthumb/main.c
@@ -694,6 +694,7 @@ gth_application_new (void)
 {
         return g_object_new (gth_application_get_type (),
                              "application-id", "org.gnome.Gthumb",
+                             "register-session", TRUE, /* required to call gtk_application_inhibit */
                              "flags", 0,
                              NULL);
 }
