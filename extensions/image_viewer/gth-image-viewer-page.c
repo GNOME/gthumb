990e40c8568f3fed8a9f7e894ba11fada49b1971
Paolo Bacchilega <paobac@src.gnome.org>
2016-01-03 11:19:23 +0100

    image viewer: load the zoom menu from memory

--- extensions/image_viewer/gth-image-viewer-page.c
+++ extensions/image_viewer/gth-image-viewer-page.c
@@ -935,7 +935,7 @@ gth_image_viewer_page_real_activate (GthViewerPage *base,
 							   "win.image-zoom-fit-if-larger",
 							   NULL);
 
-	self->priv->builder = _gtk_builder_new_from_file ("toolbar-zoom-menu.ui", "image_viewer");
+	self->priv->builder = gtk_builder_new_from_resource ("/org/gnome/gThumb/image_viewer/data/ui/toolbar-zoom-menu.ui");
 	self->priv->buttons[ZOOM_BUTTON] =
 			gth_browser_add_header_bar_menu_button (browser,
 								GTH_BROWSER_HEADER_SECTION_VIEWER_VIEW,
