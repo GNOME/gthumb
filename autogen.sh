ae5a7a215703ccc3e237868870e8dab337d50c2d
Paolo Bacchilega <paobac@src.gnome.org>
2016-03-30 11:25:19 +0200

    removed unused definitions

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gthumb"
 REQUIRED_AUTOMAKE_VERSION=1.8
 
 (test -f $srcdir/configure.ac \
@@ -19,4 +18,4 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+. gnome-autogen.sh
