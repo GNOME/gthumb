6b76a98fb05d5a4e87bdb004d276726d636affe2
Paolo Bacchilega <paobac@src.gnome.org>
2020-02-23 10:27:42 +0100

    terminal: added preferences dialog to customize the command

--- extensions/terminal/dlg-terminal-preferences.h
+++ extensions/terminal/dlg-terminal-preferences.h
@@ -3,7 +3,7 @@
 /*
  *  GThumb
  *
- *  Copyright (C) 2020 Free Software Foundation, Inc.
+ *  Copyright (C) 2020 The Free Software Foundation, Inc.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,37 +19,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef DLG_TERMINAL_PREFERENCES_H
+#define DLG_TERMINAL_PREFERENCES_H
 
-#include <config.h>
-#include <gtk/gtk.h>
 #include <gthumb.h>
-#include "callbacks.h"
-#include "dlg-terminal-preferences.h"
 
+void dlg_terminal_preferences (GtkWindow *parent);
 
-G_MODULE_EXPORT void
-gthumb_extension_activate (void)
-{
-	gth_hook_add_callback ("gth-browser-construct", 10, G_CALLBACK (terminal__gth_browser_construct_cb), NULL);
-	gth_hook_add_callback ("gth-browser-folder-tree-popup-before", 10, G_CALLBACK (terminal__gth_browser_folder_tree_popup_before_cb), NULL);
-}
-
-
-G_MODULE_EXPORT void
-gthumb_extension_deactivate (void)
-{
-}
-
-
-G_MODULE_EXPORT gboolean
-gthumb_extension_is_configurable (void)
-{
-	return TRUE;
-}
-
-
-G_MODULE_EXPORT void
-gthumb_extension_configure (GtkWindow *parent)
-{
-	dlg_terminal_preferences (parent);
-}
+#endif /* DLG_TERMINAL_PREFERENCES_H */
