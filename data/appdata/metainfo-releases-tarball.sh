bba8055d5726ac00d4e6ad96cf5a2bfd71065741
AsciiWolf <mail@asciiwolf.com>
2025-07-14 01:18:44 +0200

    data: Rename appdata to metainfo
    The latter is preferred nowadays and is used by other GNOME projects

--- data/appdata/metainfo-releases-tarball.sh
+++ data/appdata/metainfo-releases-tarball.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-appstreamcli news-to-metainfo --limit=1 $MESON_DIST_ROOT/NEWS $MESON_DIST_ROOT/data/appdata/org.gnome.gThumb.appdata.xml.in $MESON_DIST_ROOT/data/appdata/org.gnome.gThumb.appdata.xml.in
+appstreamcli news-to-metainfo --limit=1 $MESON_DIST_ROOT/NEWS $MESON_DIST_ROOT/data/appdata/org.gnome.gThumb.metainfo.xml.in $MESON_DIST_ROOT/data/appdata/org.gnome.gThumb.metainfo.xml.in
 
