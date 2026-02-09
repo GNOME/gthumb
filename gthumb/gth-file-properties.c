2af22e4af9a4414c720f10c2868c1f0db61628a8
Paolo Bacchilega <paobac@src.gnome.org>
2026-02-09 18:15:15 +0100

    File properties: fixed property truncation
    https://gitlab.gnome.org/GNOME/gthumb/-/issues/379

--- gthumb/gth-file-properties.c
+++ gthumb/gth-file-properties.c
@@ -218,8 +218,14 @@ gth_file_properties_real_set_file (GthPropertyView *base,
 			char *tmp_value;
 
 			utf8_value = _g_utf8_from_any (value);
-			if (g_utf8_strlen (utf8_value, -1) > MAX_ATTRIBUTE_LENGTH)
-				g_utf8_strncpy (g_utf8_offset_to_pointer (utf8_value, MAX_ATTRIBUTE_LENGTH - 1), "…", 1);
+			if (g_utf8_strlen (utf8_value, -1) > MAX_ATTRIBUTE_LENGTH) {
+				char *truncated = g_utf8_substring (utf8_value, 0, MAX_ATTRIBUTE_LENGTH);
+				if (truncated != NULL) {
+					g_free (utf8_value);
+					utf8_value = g_strconcat (truncated, "…", NULL);
+					g_free (truncated);
+				}
+			}
 			tmp_value = _g_utf8_replace_pattern (utf8_value, "[\r\n]", " ");
 			g_free (value);
 			value = tmp_value;
