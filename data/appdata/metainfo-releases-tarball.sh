6b2bddee675546c5ea0d0f2b18f39cfdab2a7bac
Paolo Bacchilega <paobac@src.gnome.org>
2026-03-11 21:12:03 +0100

    org.gnome.gthumb -> app.gthumb.gthumb

--- data/appdata/metainfo-releases-tarball.sh
+++ data/appdata/metainfo-releases-tarball.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-appstreamcli news-to-metainfo --limit=1 $MESON_DIST_ROOT/NEWS $MESON_DIST_ROOT/data/appdata/org.gnome.gThumb.metainfo.xml.in $MESON_DIST_ROOT/data/appdata/org.gnome.gThumb.metainfo.xml.in
-
+appstreamcli news-to-metainfo --limit=1 $MESON_DIST_ROOT/NEWS $MESON_DIST_ROOT/data/appdata/app.gthumb.gthumb.metainfo.xml.in $MESON_DIST_ROOT/data/appdata/app.gthumb.gthumb.metainfo.xml.in
