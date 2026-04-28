adf986a3c38a52c0fc3a7d29ac7d442c73a5e7b1
Paolo Bacchilega <paobac@src.gnome.org>
2026-04-28 12:27:41 +0200

    metainfo: removed script to update the release notes

--- data/appdata/metainfo-releases-tarball.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-appstreamcli news-to-metainfo --limit=1 $MESON_DIST_ROOT/NEWS $MESON_DIST_ROOT/data/appdata/org.gnome.gthumb.metainfo.xml.in $MESON_DIST_ROOT/data/appdata/org.gnome.gthumb.metainfo.xml.in
